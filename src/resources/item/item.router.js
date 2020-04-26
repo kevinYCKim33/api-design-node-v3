77c47cbbf5d81fd2a5ceec604fca8ef45a6be32e
Kevin Y. Kim <kevinyckim33@gmail.com>
2020-04-26 19:34:30 -0400

    idk why not passing

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,34 @@
 import { Router } from 'express'
 
+const controller = (req, res) => {
+  res.send({ message: 'hello' })
+}
+
 const router = Router()
+
+// have route blind to where it's mounted to
+// /api/item
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+router
+  .route('/:id')
+  .put(controller)
+  .delete(controller)
+  .get(controller)
+
+// MY ATTEMPT
+// router
+//   .route('/')
+//   .get((req, res) => res.send({ message: 'hello' }))
+//   .post((req, res) => res.send({ message: 'hello' }))
+
+// router
+//   .route('/:id')
+//   .post((req, res) => res.send({ message: 'hello' }))
+//   .put((req, res) => res.send({ message: 'hello' }))
+//   .delete((req, res) => res.send({ message: 'hello' }))
 
 export default router
