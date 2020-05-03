b57916fa81ce5b54dcfe779d7e192a40f688a1c2
Kevin Y. Kim <kevinyckim33@gmail.com>
2020-05-03 14:53:38 -0400

    just some note stuff

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -2,3 +2,11 @@ import { crudControllers } from '../../utils/crud'
 import { Item } from './item.model'
 
 export default crudControllers(Item)
+
+/*
+GET / Read many
+GET /:id Read one
+POST / Create one
+PUT /:id Update One
+DELETE /:id Delete One
+*/
--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -6,6 +6,13 @@ const router = Router()
 // /api/item
 router
   .route('/')
+  .get((req, res) => {
+    // res.status(404).send({message: 'not found'})
+    // res.status(404).json({ message: 'hello'})
+    // THINK OF RESPONSE AS RETURN!
+    // don't put anything after response
+    // don't get clever like oh i'll do analytics after responding!
+  })
   .get(controllers.getMany)
   .post(controllers.createOne)
 
