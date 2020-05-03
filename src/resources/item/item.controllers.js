b1e2356131c3cdd314a23d346c3dc50d26809a11
Kevin Y. Kim <kevinyckim33@gmail.com>
2020-05-03 15:58:21 -0400

    awesome override for a specific controller

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -2,3 +2,11 @@ import { crudControllers } from '../../utils/crud'
 import { Item } from './item.model'
 
 export default crudControllers(Item)
+
+// syntax to write a slightly modified CRUD for an items controller
+// export default {
+//     ...crudControllers(Item),
+//     getOne() {
+
+//     }
+// }
