80afd322ac293ef3af74e32594fa2041c2ed7ee4
Kevin Y. Kim <kevinyckim33@gmail.com>
2020-05-03 14:12:42 -0400

    added comments to item model creation

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -7,6 +7,8 @@ const itemSchema = new mongoose.Schema(
       required: true,
       trim: true,
       maxlength: 50
+      // unique: true; would be horrible cause the name needs to be unique across all index
+      // someone probably wrote Clean Room on their To Do List
     },
     status: {
       type: String,
@@ -16,11 +18,18 @@ const itemSchema = new mongoose.Schema(
     },
     notes: String,
     due: Date,
+    // basically how to set up relationship...
+    // this field will use the id of a user
+    // ref not req'd
+    // population.mongo ~ joining tables at runtime
+    // it's easier to read i guess...
+    // ObjectId: how mongo does id's
     createdBy: {
       type: mongoose.SchemaTypes.ObjectId,
-      ref: 'user',
+      ref: 'user', // see user.model.js export
       required: true
     },
+    // this will belong to a list!
     list: {
       type: mongoose.SchemaTypes.ObjectId,
       ref: 'list',
@@ -30,4 +39,11 @@ const itemSchema = new mongoose.Schema(
 
   { timestamps: true }
 )
+
+// compound index
+// if you try to make an item named 'Clean Room' on ToDo List #1, it will error out
+// 1 what order do you want to sort your indexes?
+// just sorting of indexes...
+// don't think too much on it...
+itemSchema.index({ list: 1, name: 1 }, { unique: true })
 export const Item = mongoose.model('item', itemSchema)
